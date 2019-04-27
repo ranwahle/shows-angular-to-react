0ac9569fb9e8de551df734ffaba476508d9aeeff
ranwahle <ran.wahle@gmail.com>
2019-04-27 16:45:01 +0300

    Stories

--- react-app/src/stories/index.js
+++ react-app/src/stories/index.js
@@ -5,8 +5,14 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import SearchShow from "../components/search-show/search-show"
+import Home from "../components/home/home";
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Home component', module).add('Search show',() => <Home>
+</Home>)
+
+storiesOf('Search show', module).add('Search show', () => <SearchShow searchTerm={'Search this'}  onSearch={action('search')}/>);
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
