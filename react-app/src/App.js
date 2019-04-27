75753ec75d590bf47224e989c2f1c5e4ee906da7
ranwahle <ran.wahle@gmail.com>
2019-04-27 11:40:23 +0300

    Starer for react app, using create-react-app
    1. Copy the HTML & css for the app component

---
+++ react-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+    return (
+        <div>
+
+            <div className="header">
+                <h1> The Angular-2-React Show</h1>
+            </div>
+
+        </div>
+
+    );
+}
+
+export default App;
